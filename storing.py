93a28560c5b65f6ccb7689089eeb4d23c1eb215b
Kirill Plotnikov <kpltnk@gmail.com>
2019-08-01 16:08:14 +0500

    Rename and update functions

--- storing.py
+++ storing.py
@@ -3,15 +3,11 @@ from datetime import datetime
 
 db = {}
 
-pl = ['Nirvana - Smells Like Teen Spirit',
-      'Nirvana - Come as You Are',
-      'Nirvana - Lithium',
-      'Nirvana - In Bloom',
-      'Nirvana - Heart-Shaped Box']
+pl = ['Smells Like Teen Spirit', 'Come as You Are', 'Lithium', 'In Bloom', 'Heart-Shaped Box']
 ids = ['hTWKbfoikeg', 'vabnZ9-ex7o', 'pkcJEvMcnEg', 'PbgKEjNBHqM', 'n6P0SitRwy8']
 
 
-def create_entry(pl, ids):
+def combine(pl, ids):
     tracks = {}
     pl = [item.lower() for item in pl]
     links = [f'youtube.com/watch?v={item}' for item in ids]
@@ -20,12 +16,12 @@ def create_entry(pl, ids):
     return tracks
 
 
-def search(keyphrase):
-    name = fetching.get_info(keyphrase, name_only=True)
-    if name.lower() in db:
-        if db['name']['date']:  # add delta
-            return db['name']['tracks']
+def process(keyphrase, db):
+    name = fetching.get_info(keyphrase, name_only=True).lower()
+    if name in db:
+        # if db['name']['date']:  # add delta
+        return db[name]['tracks']
     else:
-        # fetching.create_top()
-        tracks = create_entry(pl, ids)
+        pl, ids = fetching.create_top(keyphrase, number=10, ids_only=False)
+        tracks = combine(pl, ids)
         db.setdefault(name, {'tracks': tracks, 'date': datetime.now()})
