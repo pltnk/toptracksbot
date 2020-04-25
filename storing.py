890bb884df2030b2d24db2bfebcc19c65d411068
Kirill Plotnikov <init@pltnk.dev>
2020-04-25 16:25:34 +0500

    Remove combine coroutine
    There is no need in separate coroutine for this functionality, now it is handled inside 'process' coroutine.

--- storing.py
+++ storing.py
@@ -25,17 +25,6 @@ logging.basicConfig(
 )
 
 
-async def combine(keyphrase: str) -> str:
-    """
-    Create JSON array containing YouTube IDs of the top tracks by the given artist according to Last.fm.
-    :param keyphrase: Name of an artist or a band.
-    :return: str containing JSON array.
-    """
-    top = await fetching.create_top(keyphrase)
-    ids = json.dumps(top)
-    return ids
-
-
 def process(keyphrase: str) -> list:
     """
     Check if an entry for the given artist exists in the database, update it if it is outdated,
@@ -44,36 +33,38 @@ def process(keyphrase: str) -> list:
     :return: List of YouTube IDs.
     """
     try:
-        name = await fetching.get_name(keyphrase)
-        name = name.lower()
+        artist = await fetching.get_name(keyphrase)
+        artist = artist.lower()
     except Exception as e:
         logging.debug(
             f"An error occurred while fetching artist name from Last.fm: {e}."
         )
-        name = keyphrase.lower()
+        artist = keyphrase.lower()
     today = datetime.now()
     conn = await asyncpg.connect(dsn=DATABASE_URL)
-    record = await conn.fetch(f"SELECT * FROM top WHERE artist = '{name}'")
+    record = await conn.fetch(f"SELECT * FROM top WHERE artist = '{artist}'")
     if (
         record
-        and (today - datetime.strptime(record[0]["date"], "%Y-%m-%d")).days < VALID_FOR_DAYS
+        and (today - datetime.strptime(record[0]["date"], "%Y-%m-%d")).days
+        < VALID_FOR_DAYS
     ):
-        logging.info(f'There is an artist with the name "{name}" in the database')
+        logging.info(f'There is an artist with the name "{artist}" in the database')
         await conn.execute(
-            f"UPDATE top SET requests = requests + 1 WHERE artist = '{name}'"
+            f"UPDATE top SET requests = requests + 1 WHERE artist = '{artist}'"
         )
-        tracks = record[0]["tracks"]
+        tracks = json.loads(record[0]["tracks"])
     else:
         logging.info(
-            f'There is no artist named "{name}" in the database or the entry is older than {VALID_FOR_DAYS} days'
+            f'There is no artist named "{artist}" in the database or the entry is older than {VALID_FOR_DAYS} days'
         )
-        tracks = await combine(name)
+        tracks = await fetching.create_top(artist)
+        tracks_json = json.dumps(tracks)
         date = datetime.strftime(today, "%Y-%m-%d")
         query = f"""INSERT INTO top (artist, tracks, date, requests) 
-                    VALUES('{name}', '{tracks}', '{date}', 1)
+                    VALUES('{artist}', '{tracks_json}', '{date}', 1)
                     ON CONFLICT (artist)
-                    DO UPDATE SET tracks = '{tracks}', date = '{date}', requests = top.requests + 1"""
+                    DO UPDATE SET tracks = '{tracks_json}', date = '{date}', requests = top.requests + 1"""
         await conn.execute(query)
-        logging.info(f'Entry for "{name}" created/updated in the database')
+        logging.info(f'Entry for "{artist}" created/updated in the database')
     await conn.close()
     return json.loads(tracks)
