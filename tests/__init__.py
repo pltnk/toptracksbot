ddf763c4ecac1cd6d0c572bed8c246a6c7ca2b3c
Kirill Plotnikov <kpltnk@gmail.com>
2021-07-30 18:23:15 +0500

    Add exception for missing .env

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,10 @@ if (
     "TTBOT_LASTFM_API_KEY" not in os.environ
     or "TTBOT_YOUTUBE_API_KEY" not in os.environ
 ):
-    import dotenv
+    dotenv_path = "../.env"
+    if os.path.exists(dotenv_path):
+        import dotenv
 
-    dotenv.load_dotenv("../.env")
+        dotenv.load_dotenv("../.env")
+    else:
+        raise RuntimeError(".env file is missing at the root of the repository")
